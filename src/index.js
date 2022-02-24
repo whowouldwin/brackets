db8ee108567fb1deac767d9f362ea4baeb71b598
ValeriyaBesson <bessonova.valeriya@bk.ru>
2022-02-24 18:05:17 +0300

    init: create implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 != 0) return false; 
+
+  let stack = [];
+  const objBrackets = Object.fromEntries(bracketsConfig);
+
+  for (let index = 0; index < str.length; index++) {
+    if (objBrackets[stack[stack.length - 1]] === str [index])
+    {
+      stack.pop();
+    }
+    else if (Object.keys(objBrackets).includes(str[index])) {
+      stack.push (str[index]);
+    }
+  }
+  return stack.length === 0;
+};
